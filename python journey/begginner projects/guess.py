ac70f0d5ecdbe6df79ae6ef875d4c62912f17408
Oluwafunbi Onaeko <sbnuf.11@gmail.com>
2023-08-24 10:46:06 +0100

    simple registration project

--- python journey/begginner projects/guess.py
+++ python journey/begginner projects/guess.py
@@ -12,3 +12,18 @@ def guess(x):
             print('sorry too low')
     print('fuck you im sure you think youre smart')
 guess(40)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        guess = random.randint(low, high)
+        feedback = input(f'is{guess} too high(H), too low (L), or corret (C)')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback  == 'l':
+            low = guess + 1
+            
+    
+
